650dc9594dae865c40e0f9daab786979b846804a
luch22 <44393787+luch22@users.noreply.github.com>
2018-10-23 11:38:13 +0200

    AJOUT admin

--- RSE.NET BACKUP 23-10-18 09h49m/DAL/Models/Administrateur.cs
+++ RSE.NET BACKUP 23-10-18 09h49m/DAL/Models/Administrateur.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL.Models {
-    class Administrateur {
+namespace DAL.Models
+{
+    class Administrateur
+    {
     }
 }
