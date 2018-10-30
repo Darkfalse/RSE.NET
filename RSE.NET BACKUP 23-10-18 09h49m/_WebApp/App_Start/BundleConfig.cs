85cb72896c2ccd098a3ff9c2556aa08ca9775ad5
luch22 <44393787+luch22@users.noreply.github.com>
2018-10-30 10:00:09 +0100

    Push css plus bootstrap

--- RSE.NET BACKUP 23-10-18 09h49m/_WebApp/App_Start/BundleConfig.cs
+++ RSE.NET BACKUP 23-10-18 09h49m/_WebApp/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace _WebApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                     "~/Content/Macss.css"));
         }
     }
 }
