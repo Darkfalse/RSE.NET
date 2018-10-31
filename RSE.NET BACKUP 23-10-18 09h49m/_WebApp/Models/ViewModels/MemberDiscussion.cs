a68fd67ef9d620d95b9721d6c3975b9920874f78
luch22 <44393787+luch22@users.noreply.github.com>
2018-10-31 13:52:07 +0100

    member discussion

--- RSE.NET BACKUP 23-10-18 09h49m/_WebApp/Models/ViewModels/MemberDiscussion.cs
+++ RSE.NET BACKUP 23-10-18 09h49m/_WebApp/Models/ViewModels/MemberDiscussion.cs
@@ -8,10 +8,13 @@ namespace _WebApp.Models.ViewModels
 {
     public class MemberDiscussion
     {
-        public MessageEmployee messageEmployee { get; set; }
+        
         public Employee employee { get; set; }
 
         public IEnumerable<MessageEmployee> ListeMessageEmployees { get; set; }
+
+
+       
 
 
     }
